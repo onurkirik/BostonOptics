dd06d832370546002e34285aa02a1f62b74e69b8
Onur Kırık <kirikonurr@gmail.com>
2023-02-26 03:05:02 +0300

    PaginationInfo

--- src/ApplicationCore/Specifications/ProductsFilterSpecification.cs
+++ src/ApplicationCore/Specifications/ProductsFilterSpecification.cs
@@ -23,5 +23,10 @@ namespace ApplicationCore.Specifications
                 Query.Where(x => x.BrandId == brandId);
             }
         }
+        public ProductsFilterSpecification(int? categoryId, int? brandId, int skip, int take) : this(categoryId, brandId)
+        {
+            Query.Skip(skip).Take(take);
+        }
     }
+
 }
